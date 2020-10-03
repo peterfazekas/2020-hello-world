321f7b0a67a2914653c136614f64b794fb02748b
pfazekas <v-pfazekas@hotels.com>
2020-10-03 18:12:46 +0200

    Create init and print method for programming items

--- src/hu/tutorial/App.java
+++ src/hu/tutorial/App.java
@@ -1,7 +1,28 @@
 package hu.tutorial;
 
+import java.util.Random;
+
 public class App {
+
+    private static int[] numbers;
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        numbers = init(100, 100);
+        print();
+    }
+
+    private static int[] init(int bound, int size) {
+        Random random = new Random();
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = random.nextInt(bound - 1) + 1;
+        }
+        return numbers;
+    }
+
+    private static void print() {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("%4d", numbers[i]);
+        }
     }
 }
