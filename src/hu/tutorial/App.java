3e6e3565b2a1d2b74b504e9d3fe2f8181cd38091
pfazekas <v-pfazekas@hotels.com>
2020-10-04 08:47:06 +0200

    Introduce Stream-API

--- src/hu/tutorial/App.java
+++ src/hu/tutorial/App.java
@@ -1,8 +1,10 @@
 package hu.tutorial;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class App {
 
@@ -13,90 +15,80 @@ public class App {
 
     public static void main(String[] args) {
         numbers = init();
-        print();
+        print(numbers);
         System.out.println("1. Sorozatszámatás: A számok összege: " + summation());
         int divisor = 2;
         boolean hasItem = decision(divisor);
         System.out.println("2. Eldöntés: " + (hasItem ? "Van " : "Nincs ") + "a tömbben " + divisor + "-al osztható szám!");
         if (hasItem) {
-            System.out.println("3. Kiválasztás: A sorozat " + (selection(divisor) + 1) + ". eleme osztható " + divisor + "-al");
+            System.out.println("3. Kiválasztás: A sorozat " + selection(divisor) + " értékű osztható " + divisor + "-al");
         }
-        divisor = 3;
-        int item = search(divisor);
-        String searchResult = item > -1
-                ? "A sorozat " + (item + 1) + ". eleme osztható " + divisor + "-al."
-                : "A sorozatban nem található " + divisor + "-al osztható elem.";
+        int anotherDivisor = 3;
+        var search = search(anotherDivisor);
+        String searchResult = search.map(i -> "A sorozat " + i + " értékű osztható " + anotherDivisor + "-al")
+                .orElse("A sorozatban nem található " + anotherDivisor + "-al osztható elem.");
         System.out.println("4. Keresés: " + searchResult);
-        System.out.println("5. Megszámolás: A sorozatban " + count(divisor) + " darab " + divisor + "-al osztható elem található.");
+        System.out.println("5. Megszámolás: A sorozatban " + count(anotherDivisor) + " darab " + anotherDivisor + "-al osztható elem található.");
         int max = maxSelection();
-        System.out.println("6. Maximum kiválasztás: A sorozat legnagyobb értékeű eleme " +max);
+        System.out.println("6. Maximum kiválasztás: A sorozat legnagyobb értékeű eleme " + max);
+        System.out.println("A rendezetlen tömb elemei:");
+        print(numbers);
+        System.out.println("Rendzeve Stream-API-val:");
+        print(sort());
     }
 
     private static List<Integer> init() {
         Random random = new Random();
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < SIZE; i++) {
-            numbers.add(random.nextInt(BOUND - 1) + 1);
-        }
-        return numbers;
+        return IntStream.range(0, SIZE)
+                .mapToObj(i -> random.nextInt(BOUND - 1) + 1)
+                .collect(Collectors.toList());
     }
 
-    private static void print() {
-        for (var number : numbers) {
-            System.out.printf("%4d", number);
-        }
+    private static void print(List<Integer> list) {
+        list.forEach(i -> System.out.printf("%4d", i));
         System.out.println();
     }
 
     private static int summation() {
-        int sum = 0;
-        for (var number : numbers) {
-            sum = sum + number;
-        }
-        return sum;
+        return numbers.stream()
+                .mapToInt(i -> i)
+                .sum();
     }
 
     private static boolean decision(int divisor) {
-        int i = 0;
-        while (i < numbers.size() && !(numbers.get(i) % divisor == 0)) {
-            i++;
-        }
-        return i < numbers.size();
+        return numbers.stream()
+                .anyMatch(i -> i % divisor == 0);
     }
 
     private static int selection(int divisor) {
-        int i = 0;
-        while (!(numbers.get(i) % divisor == 0)) {
-            i++;
-        }
-        return i;
+        return numbers.stream()
+                .filter(i -> i % divisor == 0)
+                .findFirst()
+                .get();
     }
 
-    private static int search(int divisor) {
-        int i = 0;
-        while (i < numbers.size() && !(numbers.get(i) % divisor == 0)) {
-            i++;
-        }
-        return i < numbers.size() ? i : -1;
+    private static Optional<Integer> search(int divisor) {
+        return numbers.stream()
+                .filter(i -> i % divisor == 0)
+                .findFirst();
     }
 
-    private static int count(int divisor) {
-        int count = 0;
-        for (var number : numbers) {
-            if (number % divisor == 0) {
-                count++;
-            }
-        }
-        return count;
+    private static long count(int divisor) {
+        return numbers.stream()
+                .filter(i -> i % divisor == 0)
+                .count();
     }
 
     private static int maxSelection() {
-        int max = 0;
-        for (var number : numbers) {
-            if (number > max) {
-                max = number;
-            }
-        }
-        return max;
+        return numbers.stream()
+                .mapToInt(i -> i)
+                .max()
+                .getAsInt();
+    }
+
+    private static List<Integer> sort() {
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
